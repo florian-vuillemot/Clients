d053508db35327a9d41f54726d9fc5d1331177cc
vuille_f <florian.vuillemot@posteo.net>
2020-03-16 22:11:57 +0100

    Refactor test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,57 +4,61 @@ using Dates
 using Clients
 
 
-@testset "Create Client" begin
-    name = "foo"
+@testset "Client" begin
+    @testset "Constructor" begin
+        name = "foo"
 
-    @testset "From name" begin
-        client = Clients.Client(name)
+        @testset "From name" begin
+            client = Clients.Client(name)
 
-        @test client.name == name
-        @test client.dateofbirthday == nothing
-    end
+            @test client.name == name
+            @test client.dateofbirthday == nothing
+        end
 
-    @testset "With all parameters" begin
-        dateofbirthday = Date(2019)
-        client = Clients.Client(name, dateofbirthday)
+        @testset "With all parameters" begin
+            dateofbirthday = Date(2019)
+            client = Clients.Client(name, dateofbirthday)
 
-        @test client.name == name
-        @test client.dateofbirthday == dateofbirthday
+            @test client.name == name
+            @test client.dateofbirthday == dateofbirthday
+        end
     end
 end;
 
 
-@testset "Create a Horse Owner" begin
-    name = "foo"
+@testset "Horse Owner" begin
+    @testset "Constructor" begin
+        name = "foo"
 
 
-    @testset "From name" begin
-        horseowner = Clients.HorseOwner{Int}(name)
+        @testset "From name" begin
+            horseowner = Clients.HorseOwner{Int}(name)
 
-        @test horseowner.name == name
-        @test horseowner.dateofbirthday == nothing
-        @test length(horseowner.horses) == 0
-    end
+            @test horseowner.name == name
+            @test horseowner.dateofbirthday == nothing
+            @test length(horseowner.horses) == 0
+        end
 
-    @testset "With birthday" begin
-        dateofbirthday = Date(2019)
+        @testset "With birthday" begin
+            dateofbirthday = Date(2019)
 
-        horseowner = Clients.HorseOwner{Int}(name, dateofbirthday)
+            horseowner = Clients.HorseOwner{Int}(name, dateofbirthday)
 
-        @test horseowner.name == name
-        @test horseowner.dateofbirthday == dateofbirthday
-        @test length(horseowner.horses) == 0
-    end
+            @test horseowner.name == name
+            @test horseowner.dateofbirthday == dateofbirthday
+            @test length(horseowner.horses) == 0
+        end
 
-    @testset "With Horses" begin
-        nbhorses = 5
-        dateofbirthday = Date(2019)
-        horses = Vector{Int}(1:nbhorses)
+        @testset "With Horses" begin
+            nbhorses = 5
+            dateofbirthday = Date(2019)
+            horses = Vector{Int}(1:nbhorses)
 
-        horseowner = Clients.HorseOwner{Int}(name, dateofbirthday, horses)
+            horseowner = Clients.HorseOwner{Int}(name, dateofbirthday, horses)
 
-        @test horseowner.name == name
-        @test horseowner.dateofbirthday == dateofbirthday
-        @test length(horseowner.horses) == nbhorses
+            @test horseowner.name == name
+            @test horseowner.dateofbirthday == dateofbirthday
+            @test length(horseowner.horses) == nbhorses
+        end
     end
 end
